cea6541569f100ab89b3607c9b8a008211061b6c
Jack Zhao <YIHIU@SINA.COM>
2016-11-03 16:23:49 -0700

    Initial commit

---
+++ app/src/main/java/com/jackzc/intentsreceiver/IntentReceiver.java
@@ -0,0 +1,18 @@
+package com.jackzc.intentsreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class IntentReceiver extends BroadcastReceiver {
+    public IntentReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "Broadcast has been received", Toast.LENGTH_LONG).show();
+
+    }
+}
